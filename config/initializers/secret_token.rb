9c5625ce4a11ed86bc60e75f6c8dfe95affe83e5
Philip Woo <jhw1202@gmail.com>
2013-06-19 15:22:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pairmeup::Application.config.secret_token = '7c57b3d2354f76f45e4a550b37e89d8494b95285f10bb8fe544533896078cfc9fc149bb6a5fad2f4af11bf86a6f7361fd24e2903856e8c26d1661ad0e4196de3'
