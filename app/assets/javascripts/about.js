580618ed0ba9ded69fa1e5deba7950f220382068
Philip Woo <jhw1202@gmail.com>
2013-06-22 18:10:34 -0700

    minor styling and placement; js to reveal members instead of displaying them by default.

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $('.about').on('click', function(){
     $('.description').toggle('slow', function(){})
   })
+
+  $('.members-list-expand').on('click', function(){
+    var _this = $(this)
+    _this.text() === "Show members" ? _this.text("Hide members") : _this.text("Show members")
+    $(this).next().toggle('slow', function(){})
+  })
 })
