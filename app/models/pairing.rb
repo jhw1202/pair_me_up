8a9ccd3b7121173a066d848c0a4e0ab9b0efe4e3
Philip Woo <jhw1202@gmail.com>
2013-06-22 22:45:25 -0700

    working pairing version. TODO: two rake tasks to write: one is proper one for heroku scheduler, other is to simulate weeks passing and pick pairs over a few weeks.

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,7 +2,7 @@ class Pairing < ActiveRecord::Base
 
   after_create :change_timestamp
 
-  attr_accessible :member_1, :member_2
+  attr_accessible :member_1, :member_2, :team_id
 
   def change_timestamp
     self.update_attribute(:created_at, DateTime.now.beginning_of_day)
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
+$blacklist = []
+
 class Team < ActiveRecord::Base
   has_many :memberships
   has_many :members, :through => :memberships
@@ -8,9 +10,25 @@ class Team < ActiveRecord::Base
 
   # before_save :pick_pairs
 
+  def members_without_blacklist
+    members = self.members.includes(:teams)
+    filtered = []
+    members.each do |member|
+      unless $blacklist.include?(member.id)
+        filtered << member
+      end
+    end
+    filtered
+  end
+
   def pick_pairs
+    members = self.members_without_blacklist
+
+    multiple_team_members = members.select{|m| m.teams.length > 1}.map(&:id)
+
     this_week = []
-    team_pairs = remove_previous_week_pairs(self.all_possible_pairs)
+
+    team_pairs = remove_previous_week_pairs(self.all_possible_pairs(members), self.id)
 
     until this_week.length == (members.length / 2) do
       pair = team_pairs.sample
@@ -18,14 +36,16 @@ class Team < ActiveRecord::Base
       this_week << pair
     end
 
-    create_pairs_log(this_week)
+    self.create_pairs_log(this_week)
+
+    blacklist_duplicates(this_week, $blacklist, multiple_team_members)
 
     p this_week
 
   end
 
-  def all_possible_pairs
-    members = self.members
+  def all_possible_pairs(members)
+    members = members
     team_pairs = []
 
     members.each_with_index do |person, index|
@@ -46,22 +66,25 @@ class Team < ActiveRecord::Base
     updated_pairs
   end
 
-  ########################################################
-  ## CHANGE THIS METHOD SO IT USES FOR LAST WEEKS DATA ##
-  ########################################################
-
-  def remove_previous_week_pairs(team_pairs)
-    last_week = Pairing.find(:all, :order => "id DESC", :limit => 2)
+  def remove_previous_week_pairs(team_pairs, team_id)
+    last_week = Pairing.where("team_id = ? AND created_at = ?",
+                               team_id, (DateTime.now - 1.week).beginning_of_day)
     team_pairs - last_week.map(&:pair)
   end
 
-  ########################################################
-  ########################################################
-
-
   def create_pairs_log(pairs)
     pairs.each do |pair|
-      Pairing.create(member_1: pair[0], member_2: pair[1])
+      Pairing.create(member_1: pair[0], member_2: pair[1], team_id: self.id)
+    end
+  end
+
+  def blacklist_duplicates(selected_pairs, blacklist, multiple_team_members)
+    selected_pairs.each do |pair|
+      if multiple_team_members.include? pair[0]
+        blacklist << pair[0]
+      elsif multiple_team_members.include? pair[1]
+        blacklist << pair[1]
+      end
     end
   end
 
