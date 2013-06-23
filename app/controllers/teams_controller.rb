fc14f4c665bcb8ae490e77fd32d78ba2d84eb686
Philip Woo <jhw1202@gmail.com>
2013-06-22 18:39:52 -0700

    partialed out some html. wrote code for creating and deleting teams. flash needs to be fixed, but skip for now.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,10 +8,14 @@ class TeamsController < ApplicationController
   end
 
   def create
-
+    Team.create(params[:team])
+    flash[:notice] = "Successfully created a new team"
+    redirect_to root_path
   end
 
   def destroy
-
+    team = Team.find(params[:id]).destroy
+    flash[:notice] = "Team deleted. If a member belongs to multiple teams, only their membership to this deleted team is destroyed."
+    redirect_to root_path
   end
 end
