242954eab44a40e675494b491d734f9e55fdb932
Philip Woo <jhw1202@gmail.com>
2013-06-23 15:32:24 -0700

    time zone configure so rails uses local time zone setting ie pacific time

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,7 @@ module Pairmeup
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.active_record.default_timezone = :local
     # Enable the asset pipeline
     config.assets.enabled = true
 
